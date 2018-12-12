72fc4e2ce29e9933a97f469199786d03e612d676
nature1995 <gzr2008@vip.qq.com>
2018-12-12 01:00:59 -0500

    Add Project01
    Add Project01

---
+++ [project01]-tictactoe/lib/game_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GameButton{
+  final id;
+  String text;
+  Color bg;
+  bool enabled;
+
+  GameButton({this.id, this.text = "", this.bg = Colors.grey, this.enabled = true});
+}
